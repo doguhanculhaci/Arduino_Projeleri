9ceaec5518027b15359f2d598e6e6202074daed1
Doğuhan Çulhacıoğlu <53540561+doguhanculhaci@users.noreply.github.com>
2021-05-06 01:23:40 +0300

    Add files via upload

---
+++ harekete_duyarli_klavye_pc_control/harekete_duyarli_klavye_pc_control/harekete_duyarli_klavye_pc_control.ino
@@ -0,0 +1,65 @@
+#include <Keyboard.h> 
+
+int dakika = 0;
+int saniye = 0;
+
+int pirPin = 9;
+bool hareket_algilandi_mi = false;
+
+void setup ()
+{
+Keyboard.begin ();
+pinMode(pirPin, INPUT);
+delay(1000);;
+}
+
+void loop ()
+{
+  hareket = digitalRead(pirPin);
+
+  if(hareket == HIGH)
+  {
+    hareket_algilandi_mi = true;
+  }
+
+  if(hareket_algilandi_mi)
+  {
+    Keyboard.press (KEY_LEFT_CTRL);
+    delay(100);
+    Keyboard.press (KEY_LEFT_ALT);
+    delay(100);
+    Keyboard.println ('O');
+    Keyboard.releaseAll ();
+    delay(5000);
+    
+    Keyboard.println ('ASDASD');
+    delay (100);
+    Keyboard.write (KEY_RETURN);
+    delay(1000);
+    
+    Keyboard.println ('f');
+    delay (100);
+    Keyboard.write (KEY_RETURN);
+
+    for(int k = 0; k < saniye; k++)
+    {
+      delay(1000);
+    }
+
+    for(int i = 0; i <= dakika; i++)
+    {
+      for(int j = 0; j <= 60; j++)
+      {
+        delay(1000);
+      }
+    }
+
+    Keyboard.press (KEY_LEFT_ALT);
+    delay(100);
+    Keyboard.press (KEY_F4);
+    Keyboard.releaseAll ();
+
+    hareket_algilandi_mi = false;
+  }
+  
+}
